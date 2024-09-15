4480303bfd96bd9112660aa94fae5b1caa12b69d
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-15 21:57:39 +0200

    feat: create Food classes

---
+++ src/models/food.ts
@@ -0,0 +1,66 @@
+export abstract class Food {
+    protected name: string;
+    protected price: number;
+    protected nutrition: number;
+    protected hydration: number;
+
+    getName(): string {
+        return this.name;
+    }
+
+    getPrice(): number {
+        return this.price;
+    }
+
+    getNutrition(): number {
+        return this.nutrition;
+    }
+
+    getHydration(): number {
+        return this.hydration;
+    }
+}
+
+export class Snack extends Food {
+    constructor() {
+        super();
+
+        this.name = 'snack';
+        this.price = 100;
+        this.nutrition = 20;
+        this.hydration = 0;
+    }
+}
+
+export class Drink extends Food {
+    constructor() {
+        super();
+
+        this.name = 'drink';
+        this.price = 100;
+        this.nutrition = 0;
+        this.hydration = 20;
+    }
+}
+
+export class Lunch extends Food {
+    constructor() {
+        super();
+
+        this.name = 'lunch';
+        this.price = 300;
+        this.nutrition = 20;
+        this.hydration = 15;
+    }
+}
+
+export class BuffetLunch extends Food {
+    constructor() {
+        super();
+
+        this.name = 'buffet-lunch';
+        this.price = 400;
+        this.nutrition = 20;
+        this.hydration = 10;
+    }
+}
