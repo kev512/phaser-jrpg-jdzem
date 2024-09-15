fd16ef4182551431ad94d8be91467969b0e00865
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-15 21:54:11 +0200

    feat: create Worker consts

---
+++ src/models/worker.consts.ts
@@ -0,0 +1,8 @@
+export const MAX_HUNGER = 100;
+export const MAX_THIRST = 100;
+export const MAX_URINE = 100;
+export const MAX_POOP = 100;
+export const MAX_STRESS = 100;
+export const MAX_FATIGUE = 100;
+
+export const INITIAL_CASH = 100;
