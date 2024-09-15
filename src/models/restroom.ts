e5a4c49ee05a54e04bead105b2bc3145ee426275
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-15 22:00:12 +0200

    feat: create Restroom classes

---
+++ src/models/restroom.ts
@@ -0,0 +1,37 @@
+export abstract class Restroom {
+    protected name: string;
+    protected urination: number;
+    protected excrement: number;
+
+    getName(): string {
+        return this.name;
+    }
+
+    getUrinationValue(): number {
+        return this.urination;
+    }
+
+    getExcrementValue(): number {
+        return this.excrement;
+    }
+}
+
+export class Pissoir extends Restroom {
+    constructor() {
+        super();
+
+        this.name = 'pissoir';
+        this.urination = 100;
+        this.excrement = 0;
+    }
+}
+
+export class Toilet extends Restroom {
+    constructor() {
+        super();
+
+        this.name = 'toilet';
+        this.urination = 100;
+        this.excrement = 100;
+    }
+}
