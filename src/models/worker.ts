6dc342abe3feed52e46cc8a9016a150aa807f7d3
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-16 19:56:33 +0200

    feat(worker): speed property, limit values by consts

--- src/models/worker.ts
+++ src/models/worker.ts
@@ -1,3 +1,4 @@
+import { INITIAL_SPEED, MAX_FATIGUE, MAX_HUNGER, MAX_POOP, MAX_SPEED, MAX_STRESS, MAX_THIRST, MAX_URINE } from './consts/worker.consts';
 import { Effect } from './effects';
 import { Resources } from './resources';
 
@@ -8,18 +9,19 @@ export class Worker {
   private poop: number = 0;
   private stress: number = 0;
   private fatigue: number = 0;
+  private speed: number = INITIAL_SPEED;
 
   private resources: Resources;
 
   constructor() {}
 
   applyEffect(effect: Effect) {
-    this.hunger = Math.max(0, this.hunger - effect.getEffectHuger());
-    this.thirst = Math.max(0, this.thirst - effect.getEffectThirst());
-    this.urine = Math.max(0, this.urine - effect.getEffectUrine());
-    this.poop = Math.max(0, this.poop - effect.getEffectPoop());
-    this.stress = Math.max(0, this.stress - effect.getEffectStress());
-    this.fatigue = Math.max(0, this.fatigue - effect.getEffectFatigue());
-    this.fatigue = Math.max(0, this.fatigue - effect.getEffectFatigue());
+    this.hunger = Math.min(MAX_HUNGER, Math.max(0, this.hunger - effect.getEffectHuger()));
+    this.thirst = Math.min(MAX_THIRST, Math.max(0, this.thirst - effect.getEffectThirst()));
+    this.urine = Math.min(MAX_URINE, Math.max(0, this.urine - effect.getEffectUrine()));
+    this.poop = Math.min(MAX_POOP, Math.max(0, this.poop - effect.getEffectPoop()));
+    this.stress = Math.min(MAX_STRESS, Math.max(0, this.stress - effect.getEffectStress()));
+    this.fatigue = Math.min(MAX_FATIGUE, Math.max(0, this.fatigue - effect.getEffectFatigue()));
+    this.speed = Math.min(MAX_SPEED, Math.max(0, this.speed - effect.getEffectSpeed()));
   }
 }
