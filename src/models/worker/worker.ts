1a2433b035728d62fd20fa8ce1f6f7731c56c8da
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-16 21:16:03 +0200

    fix: operator fix

--- src/models/worker/worker.ts
+++ src/models/worker/worker.ts
@@ -27,31 +27,33 @@ export class Worker {
   applyEffect(effect: Effect) {
     this.hunger = Math.min(
       MAX_HUNGER,
-      Math.max(0, this.hunger - effect.getEffectHuger()),
+      Math.max(0, this.hunger + effect.getEffectHunger()),
     );
     this.thirst = Math.min(
       MAX_THIRST,
-      Math.max(0, this.thirst - effect.getEffectThirst()),
+      Math.max(0, this.thirst + effect.getEffectThirst()),
     );
     this.urine = Math.min(
       MAX_URINE,
-      Math.max(0, this.urine - effect.getEffectUrine()),
+      Math.max(0, this.urine + effect.getEffectUrine()),
     );
     this.poop = Math.min(
       MAX_POOP,
-      Math.max(0, this.poop - effect.getEffectPoop()),
+      Math.max(0, this.poop + effect.getEffectPoop()),
     );
     this.stress = Math.min(
       MAX_STRESS,
-      Math.max(0, this.stress - effect.getEffectStress()),
+      Math.max(0, this.stress + effect.getEffectStress()),
     );
     this.fatigue = Math.min(
       MAX_FATIGUE,
-      Math.max(0, this.fatigue - effect.getEffectFatigue()),
+      Math.max(0, this.fatigue + effect.getEffectFatigue()),
     );
     this.speed = Math.min(
       MAX_SPEED,
-      Math.max(0, this.speed - effect.getEffectSpeed()),
+      Math.max(0, this.speed + effect.getEffectSpeed()),
     );
+
+    this.resources.applyEffect(effect);
   }
 }
