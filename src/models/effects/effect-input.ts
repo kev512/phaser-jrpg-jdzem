736f551ef764490fd9d9cc5d22f3e5a2f01f3c67
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-16 22:25:47 +0200

    feat: create EffectInput type

---
+++ src/models/effects/effect-input.ts
@@ -0,0 +1,14 @@
+export type EffectInput = {
+  hunger?: number;
+  thirst?: number;
+  urine?: number;
+  poop?: number;
+  stress?: number;
+  fatigue?: number;
+  cash?: number;
+  reputation?: number;
+  speed?: number;
+  diapers?: number;
+  beers?: number;
+  smokes?: number;
+};
