e3cd178049d200ecf85bf212a81a5cf56dc6c715
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-16 20:22:40 +0200

    feat(event): update properties, add getters

--- src/models/events/event.ts
+++ src/models/events/event.ts
@@ -1,7 +1,24 @@
-import { Effect } from '../effects';
+import { Effect } from './effects';
 
 export abstract class Event {
-  private effect: Effect;
+  constructor(
+    private id: string,
+    private name: string,
+    private description: string,
+    private effect: Effect,
+  ) {}
+
+  getId() {
+    return this.id;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getDescription() {
+    return this.description;
+  }
 
   getEffect() {
     return this.effect;
