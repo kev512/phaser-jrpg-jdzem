2de80b74583e80befed47c74fdfce88a10709559
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-17 20:29:09 +0200

    feat: preload textures

--- src/core/scenes/Boot.ts
+++ src/core/scenes/Boot.ts
@@ -6,10 +6,11 @@ export class Boot extends Scene {
   }
 
   preload() {
-    //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
-    //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
+    this.load.setPath('assets');
 
-    this.load.image('background', 'assets/bg.png');
+    this.load.image('logo', 'logo.png');
+    this.load.image('background', 'bg.png');
+    this.load.image('worker', 'worker.png');
   }
 
   create() {
