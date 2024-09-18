b3b9a64226e14ea7faf46b4d8b21fc0c456fef63
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-18 21:46:01 +0200

    feat: add map boundary const

--- src/core/consts.ts
+++ src/core/consts.ts
@@ -2,3 +2,10 @@ export const WIDTH = 1216;
 export const HEIGHT = 768;
 
 export const TILE_SIZE = 64;
+
+export const MAP_BOUNDARY = {
+  x: 0,
+  y: TILE_SIZE * 2,
+  width: WIDTH,
+  height: HEIGHT,
+};
