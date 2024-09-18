653ab2fb59a37888d601c449b0f40f552d883b79
DESKTOP-CG6579N\Kewin Plewa <kewin5@onet.eu>
2024-09-18 09:21:24 +0200

    draft of main menu design

--- src/core/scenes/Boot.ts
+++ src/core/scenes/Boot.ts
@@ -8,8 +8,13 @@ export class Boot extends Scene {
   preload() {
     //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
     //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
+    this.load.image('background', 'assets/bg-main-menu-example.png');
 
-    this.load.image('background', 'assets/bg.png');
+    this.load.image('background', 'path_to_background.png');
+    this.load.spritesheet('buttonBorder', 'path_to_your_spritesheet.png', {
+      frameWidth: 64,
+      frameHeight: 64 
+    });
   }
 
   create() {
