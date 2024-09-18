f77782a2bed034da88bb008ef1cae64849cdf628
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-18 20:37:08 +0200

    fix: increase worker initial speed

--- src/models/worker/worker.consts.ts
+++ src/models/worker/worker.consts.ts
@@ -7,6 +7,6 @@ export const MAX_FATIGUE = 100;
 export const MAX_SPEED = 100;
 export const MAX_DRUNKNESS = 100;
 
-export const INITIAL_SPEED = 10;
+export const INITIAL_SPEED = 200;
 
 export const WAGE = 100;
