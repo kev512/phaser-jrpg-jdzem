cd949a7a7dbc8714477f15c6f47c4e78231908a9
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-19 20:37:20 +0200

    feat(map-loader): #createMap

--- src/core/map-loader.ts
+++ src/core/map-loader.ts
@@ -1,1 +1,39 @@
-export class MapLoader {}
+import { isNull } from 'lodash';
+import canteenJson from '../../public/assets/canteen.json';
+
+export class MapLoader {
+  constructor() {}
+
+  static createMap(scene: Phaser.Scene, name: 'Canteen') {
+    let json: TiledJson;
+
+    if (name === 'Canteen') {
+      json = canteenJson;
+    } else {
+      throw new Error('Incorrect name');
+    }
+
+    return this._createMap(scene, json);
+  }
+
+  private static _createMap(scene: Phaser.Scene, json: TiledJson) {
+    const map = scene.make.tilemap({
+      data: json.layers.map((layer) => layer.data),
+      tileWidth: 16,
+      tileHeight: 16,
+    });
+
+    json.layers.forEach((item, index) => {
+      const name = item.name;
+      const tiles = map.addTilesetImage(name);
+
+      if (isNull(tiles)) {
+        throw new Error(`Tileset ${name} is null`);
+      }
+
+      map.createLayer(index, tiles, 0, 0);
+    });
+
+    return map;
+  }
+}
