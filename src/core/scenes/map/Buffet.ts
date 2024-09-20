f9f215c68e7a1b4036e2b1f727265606037d27e4
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-21 01:47:03 +0200

    feat: add buy buffet lunch popup

--- src/core/scenes/map/Buffet.ts
+++ src/core/scenes/map/Buffet.ts
@@ -1,10 +1,15 @@
 import { isNull } from 'lodash';
 import { model } from '../../../main';
+import { BuffetLunch } from '../../../models/effects/events/map/buffet/buffet-lunch';
 import { MAP_BOUNDARY, WORKER_SIZE_SCALE } from '../../consts';
 import { MapLoader } from '../../map-loader';
 import { BaseScene } from './BaseScene';
 
 export class Buffet extends BaseScene {
+  buffetLunchPopup: Phaser.GameObjects.Image;
+  buffetLunchText: Phaser.GameObjects.Text;
+  isNearChef: boolean = false;
+
   constructor() {
     super('Buffet');
   }
@@ -33,6 +38,9 @@ export class Buffet extends BaseScene {
     this.cursors = this.input.keyboard.createCursorKeys();
 
     super.createLabels();
+    super.createWindow();
+
+    this.createBuyBuffetLunchPopup();
   }
 
   update(time: number, deltaTime: number) {
@@ -47,6 +55,13 @@ export class Buffet extends BaseScene {
     }
 
     this.updateLabels();
+    this.updateWindow();
+
+    this.isNearChef =
+      this.player.y === 360 && this.player.x >= 480 && this.player.x <= 530;
+    this.updateBuyBuffetLunchPopup(this.isNearChef && !model.window.visible);
+
+    console.log(this.player.x, this.player.y);
   }
 
   private createMap() {
@@ -71,5 +86,46 @@ export class Buffet extends BaseScene {
   private starCanteenScene() {
     this.scene.start('Canteen');
     model.setScene('Canteen');
+  }
+
+  private createBuyBuffetLunchPopup() {
+    const popupX = 500;
+    const popupY = 150;
+
+    this.buffetLunchPopup = this.add.image(popupX, popupY, 'popup');
+    this.buffetLunchPopup.setScrollFactor(0);
+    this.buffetLunchText = this.add.text(
+      popupX - 86,
+      popupY - 10,
+      'Kup obiad [E]',
+      {
+        fontFamily: 'Pixelify Sans',
+        fontSize: 20,
+        color: '#000000',
+        stroke: '#dddddd',
+        strokeThickness: 2,
+      },
+    );
+    this.buffetLunchText.setScrollFactor(0);
+
+    this.buffetLunchPopup.setVisible(false);
+    this.buffetLunchText.setVisible(false);
+
+    this.input.keyboard?.on('keydown-E', () => {
+      if (this.isNearChef) {
+        model.window.visible = true;
+        model.window.title = 'Kup jedzenie';
+        model.window.description =
+          'W ten dzień możesz kupić danie dnia!\n\n' +
+          '1. Kup i zjedz danie dnia [1]\n' +
+          '2. Wyjście [ESC]';
+        model.window.options = [new BuffetLunch()];
+      }
+    });
+  }
+
+  private updateBuyBuffetLunchPopup(visible: boolean) {
+    this.buffetLunchPopup.setVisible(visible);
+    this.buffetLunchText.setVisible(visible);
   }
 }
