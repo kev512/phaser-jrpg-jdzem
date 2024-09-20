92505c42eee43e2156507f0786987df7b4d3bc2c
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-20 14:44:48 +0200

    feat: add status labels

--- src/core/scenes/map/BaseScene.ts
+++ src/core/scenes/map/BaseScene.ts
@@ -10,35 +10,22 @@ export abstract class BaseScene extends Phaser.Scene {
   protected thirst: Phaser.GameObjects.Text;
   protected urine: Phaser.GameObjects.Text;
   protected poop: Phaser.GameObjects.Text;
+  protected stress: Phaser.GameObjects.Text;
+  protected fatigue: Phaser.GameObjects.Text;
+  protected drunkness: Phaser.GameObjects.Text;
 
   constructor(name: string) {
     super(name);
   }
 
   createLabels() {
-    this.hunger = this.add.text(916, 32, '', {
-      fontSize: '18px',
-      stroke: '#ffffff',
-    });
-    this.hunger.setScrollFactor(0);
-
-    this.thirst = this.add.text(916, 32 * 2, '', {
-      fontSize: '18px',
-      stroke: '#ffffff',
-    });
-    this.thirst.setScrollFactor(0);
-
-    this.urine = this.add.text(916, 32 * 3, '', {
-      fontSize: '18px',
-      stroke: '#ffffff',
-    });
-    this.urine.setScrollFactor(0);
-
-    this.poop = this.add.text(916, 32 * 4, '', {
-      fontSize: '18px',
-      stroke: '#ffffff',
-    });
-    this.poop.setScrollFactor(0);
+    this.hunger = this.createLabel(916, 32);
+    this.thirst = this.createLabel(916, 32 * 2);
+    this.urine = this.createLabel(916, 32 * 3);
+    this.poop = this.createLabel(916, 32 * 4);
+    this.stress = this.createLabel(916, 32 * 5);
+    this.fatigue = this.createLabel(916, 32 * 6);
+    this.drunkness = this.createLabel(916, 32 * 7);
   }
 
   updateLabels() {
@@ -46,5 +33,22 @@ export abstract class BaseScene extends Phaser.Scene {
     this.thirst.setText('Pragnienie: ' + model.worker.getThirst());
     this.urine.setText('Pęcherz: ' + model.worker.getUrine());
     this.poop.setText('Dwójeczka: ' + model.worker.getPoop());
+    this.stress.setText('Stres: ' + model.worker.getStress());
+    this.fatigue.setText('Zmęczenie: ' + model.worker.getFatigue());
+    this.drunkness.setText('Upojenie: ' + model.worker.getDrunkness());
+  }
+
+  private createLabel(x: number, y: number) {
+    const label = this.add.text(x, y, '', {
+      fontFamily: 'Pixelify Sans',
+      fontSize: 28,
+      color: '#ffffff',
+      stroke: '#000000',
+      strokeThickness: 2,
+    });
+
+    label.setScrollFactor(0);
+
+    return label;
   }
 }
