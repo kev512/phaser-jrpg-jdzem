b782559c3feecc36c24707ee147711600f031aeb
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-21 00:53:12 +0200

    feat: implement #getEatEvent

--- src/models/effects/items/food-item.ts
+++ src/models/effects/items/food-item.ts
@@ -1,5 +1,6 @@
 import { Effect } from '../effects';
 import { Item } from './item';
+import { Event } from '../events/event';
 
 export abstract class FoodItem extends Item {
   constructor(
@@ -10,5 +11,15 @@ export abstract class FoodItem extends Item {
     price: number,
   ) {
     super(id, name, description, effect, price);
+  }
+
+  getEatEvent(): Event {
+    return new Eat(this.getName(), this.getDescription(), this.getEffect());
+  }
+}
+
+class Eat extends Event {
+  constructor(name: string, description: string, effect: Effect) {
+    super('EAT', name, description, effect);
   }
 }
