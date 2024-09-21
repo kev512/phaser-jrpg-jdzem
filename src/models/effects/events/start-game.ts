1ab2e31e911e7b8d33986d64ba638cebbe765447
Krzysztof Pająk <chris.araneo@gmail.com>
2024-09-21 20:36:53 +0200

    feat: create start game event

---
+++ src/models/effects/events/start-game.ts
@@ -0,0 +1,23 @@
+import { MAX_FATIGUE, MAX_HUNGER, MAX_POOP, MAX_STRESS, MAX_THIRST, MAX_URINE } from '../../worker/worker.consts';
+import { Effect } from '../effects';
+import { Event } from './event';
+
+export const id = 'START_GAME';
+
+export class StartGame extends Event {
+  constructor() {
+    super(
+      id,
+      'Witaj',
+      'Rozpoczynasz grę.',
+      new Effect({
+        hunger: -MAX_HUNGER,
+        thirst: -MAX_THIRST,
+        urine: -MAX_URINE,
+        poop: -MAX_POOP,
+        stress: -MAX_STRESS,
+        fatigue: -MAX_FATIGUE,
+      }),
+    );
+  }
+}
