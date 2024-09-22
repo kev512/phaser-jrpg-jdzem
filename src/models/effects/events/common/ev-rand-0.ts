63393ee42a2151908376215cc9fbd6142fb49685
banaszek.letoslaw <banaszek.stanislaw@gmail.com>
2024-09-22 16:24:07 +0200

    Added new random common events to test

---
+++ src/models/effects/events/common/ev-rand-0.ts
@@ -0,0 +1,18 @@
+import { Effect } from '../../effects';
+import { Event } from '../event';
+
+export const id = 'REV0'; //random event 0
+
+export class EvRand0 extends Event {
+  constructor() {
+    super(
+      id,
+      'Randomowy event 0',
+      'Dostałeś ochrzan od szefa\nbo zauważył, że nie starałeś się\nw trakcie pracy. Poczułeś jak ze stresu\nkrecik zapukał w taborecik.',
+      new Effect({
+        stress:15,
+        poop:25,
+      }),
+    );
+  }
+}
